466113c1040eba80fbb6a4cb8b3fe9f7b34bc987
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-06-18 15:10:36 -0400

    add all

---
+++ public/src/components/Banner.js
@@ -0,0 +1,34 @@
+// src/components/Banner.js
+import React from 'react';
+
+const Banner = ({ image, title }) => {
+  return (
+    <div style={bannerStyle}>
+      <img src={image} alt={title} style={imageStyle} />
+      <h1 style={titleStyle}>{title}</h1>
+    </div>
+  );
+};
+
+const bannerStyle = {
+  position: 'relative',
+  textAlign: 'center',
+  color: 'white',
+};
+
+const imageStyle = {
+  width: '100%',
+  height: 'auto',
+};
+
+const titleStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  fontSize: '3rem',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  padding: '10px',
+};
+
+export default Banner;
--- public/src/pages/Home.js
+++ public/src/pages/Home.js
@@ -1,14 +1,13 @@
 // src/pages/Home.js
 import React from 'react';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <main>
-        <h2>Welcome to the Home Page</h2>
-      </main>
+      <Banner image="/assets/banner.jpg" title="Featured Video" />
     </div>
   );
 };
