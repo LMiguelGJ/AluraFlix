d667128490de3215b089315aa66405751fe3f085
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-06-25 08:09:19 -0700

    Se realizaron ajustes en el diseño, clases y validaciones de los campos, incluyendo cambios en tamaño, altura, colores, enfoque, texto, y la creación de una nueva página de video

--- src/components/optionList/OptionList.jsx
+++ src/components/optionList/OptionList.jsx
@@ -1,22 +1,56 @@
+import { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { IoMdArrowDropdown } from "react-icons/io";
 import './OptionList.css';
 
-const OptionList = ({ value, onChange, options }) => {
+const OptionList = ({ value, onChange, options, clase, clase2 }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    const handleToggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleSelectOption = (option) => {
+        onChange({ target: { name: 'team', value: option } });
+        setIsOpen(false);
+    };
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className="option-list">
+        <div className="option-list" ref={dropdownRef}>
             <label>Categoría:</label>
-            <select
-                className="modal-form-input" // Aquí se aplica la clase para el estilo
-                value={value}
-                onChange={onChange}
-            >
-                <option value="" disabled defaultValue="" hidden>Seleccionar categoría</option>
-                {options.map((option) => (
-                    <option key={option.id} value={option.name}>
-                        {option.name}
-                    </option>
-                ))}
-            </select>
+            <div className="input-with-icon" onClick={handleToggleDropdown}>
+                <div className={`${clase}`}>
+                    {value || "Seleccionar categoría"}
+                </div>
+                <IoMdArrowDropdown className="dropdown-icon dropdown-icon-video" />
+            </div>
+            {isOpen && (
+                <ul className="dropdown-options">
+                    {options.map((option) => (
+                        <li
+                            key={option.id}
+                            className={`${clase2}`}
+                            onClick={() => handleSelectOption(option.name)}
+                        >
+                            {option.name}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
@@ -24,6 +58,8 @@ const OptionList = ({ value, onChange, options }) => {
 OptionList.propTypes = {
     value: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
+    clase: PropTypes.string.isRequired,
+    clase2: PropTypes.string.isRequired,
     options: PropTypes.arrayOf(
         PropTypes.shape({
             id: PropTypes.number.isRequired,
