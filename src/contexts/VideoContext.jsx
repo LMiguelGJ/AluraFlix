6c22e06c63469d3bb183724c4b8cab73285cdc1a
Luis Miguel <l.miguel.gozalez.j@gmail.com>
2024-06-26 14:36:15 -0400

    despliege

--- src/contexts/VideoContext.jsx
+++ src/contexts/VideoContext.jsx
@@ -10,7 +10,7 @@ export const VideoProvider = ({ children }) => {
 
     const fetchVideos = async () => {
         try {
-            const response = await fetch('http://localhost:3000/videos');
+            const response = await fetch('https://my-json-server.typicode.com/LMiguelGJ/AluraFlix/videos');
             const data = await response.json();
             setVideos(data);
         } catch (error) {
